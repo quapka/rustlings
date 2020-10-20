093654e6b5204cae7a8e89ab8ca7c6b0e339ea2d
quapka <quapka@gmail.com>
2020-10-20 22:14:33 +0200

    Update with my own solutions so far

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -6,9 +6,14 @@
 // even after you already figured it out. If you got everything working and
 // feel ready for the next exercise, remove the `I AM NOT DONE` comment below.
 
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
+    let mut x = 5;
+    let y = x;
+    x = 2;
+    let z = x;
+    let mut yy = z;
     println!("x has the value {}", x);
+    println!("x has the value {}", y);
+    println!("x has the value {}", z);
+    println!("x has the value {}", yy);
 }
